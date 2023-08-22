5dce803f33be28c907739175eaf75969c768858f
YzWu-VaalRookie <141818744+YzWu-VaalRookie@users.noreply.github.com>
2023-08-22 22:32:29 +0800

    Update bits_operation.c

--- bits_operation.c
+++ bits_operation.c
@@ -18,42 +18,48 @@ int main(void)
 	printf("num=1 counts=%d\n", cnt);
 	*/
 	
-	/*//Numbers of 1 Bits (¥u­­¥¿¾ã¼Æ)
+	/*
+ 	//Numbers of 1 Bits (Â¥uÂ­Â­Â¥Â¿Â¾Ã£Â¼Ã†)
 	int x = 11, k = 0;
     
-    while(x > 0){
-        k = k*10 + x%2;
-        x /= 2;
-        printf("k = %d\n", k);
-    }*/
+	while(x > 0)
+ 	{
+		k = k*10 + x%2;
+		x /= 2;
+		printf("k = %d\n", k);
+	}
+ 	*/
     
-    /*
+	/*
 	//Numbers of 1 Bits () 
 	int i, x = 11, res = 0; 
     
-    for(i = 0 ; i < 8 ; i++){
-        res = res + (x & 1);
-        x = x >> 1;
-    }
-    printf("res = %d\n", res);*/
+	for(i = 0 ; i < 8 ; i++)
+ 	{
+		res = res + (x & 1);
+		x = x >> 1;
+	}
+	printf("res = %d\n", res);
+ 	*/
 	
 	//Numbers of 1 Bits () 
 	int i, x = 4, res = 0, j; //x = 2147483647
-    char str[128]={0};
+	char str[128]={0};
 
 	printf("input numbers = %d\n", x);
 
-    for(i = 0, j = 32-1 ; i < 32 ; i++){
-        res = res + (x & 1);
-        str[j--] = (x & 1);
-        x = x >> 1;
-    }
+	for(i = 0, j = 32-1 ; i < 32 ; i++)
+	{
+        	res = res + (x & 1);
+        	str[j--] = (x & 1);
+        	x = x >> 1;
+    	}
 	
-    printf("Numbers of 1 Bits__result = %d\n", res);
-    printf("str[] = ");
+	printf("Numbers of 1 Bits__result = %d\n", res);
+	printf("str[] = ");
 	
 	for(i = 0 ; i < 32 ; i++)
-        printf("%d, ", str[i]);
+        	printf("%d, ", str[i]);
 	printf("\n");
     
 	system("pause");
